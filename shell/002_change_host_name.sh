76735f37e2fc9bcad7a2ef49dcbc25c0857f39a5
松野貞之 <ma2no@ma2no.local>
2020-03-15 18:25:20 +0900

    add shell

---
+++ shell/002_change_host_name.sh
@@ -0,0 +1,10 @@
+scutil --get ComputerName
+scutil --get LocalHostName
+scutil --get HostName
+
+sudo scutil --set ComputerName ma2no
+sudo scutil --set LocalHostName ma2no
+
+scutil --get ComputerName
+scutil --get LocalHostName
+scutil --get HostName
